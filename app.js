01416072f05c84366cc666103057af797eaecab4
Joe <joe@appfog.com>
2012-08-13 22:27:36 -0700

    test 06

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var app = require('express').createServer();
 app.get('/', function(req, res) {
-    res.send('Testing 05');
+    res.send('Testing 06');
 });
 app.listen(process.env.VCAP_APP_PORT || 3000);
